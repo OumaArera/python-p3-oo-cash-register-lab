a969fe21048f05f4ee936901505b7bb495d573b6
OumaArera <johnouma999@gmail.com>
2024-03-20 13:45:20 +0300

    complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,30 +1,24 @@
 class CashRegister:
-    def __init__(self, discount=0):
-        self.total = 0
-        self.discount = discount
-        self.items = [] 
 
-    def add_item(self, title, price, quantity=1):  
-        for _ in range(quantity):
-          self.items.append(title) 
-        self.total += price * quantity  
+    def __init__(self, discount = 0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction = 0
+
+    def add_item(self, title, price, quantity = 1):
+        for i in range(quantity):
+            self.items.append(title)
+
+        self.last_transaction = price * quantity
+        self.total += price * quantity
 
     def apply_discount(self):
-        if self.discount > 0:
-            discount_amount = (self.discount / 100) * self.total
-            self.total -= discount_amount
-            print("After the discount, the total comes to ${:.0f}.".format(self.total))  
-            return "After the discount, the total is: ${:.2f}".format(self.total)
-        else:
+        if self.discount == 0:
             print("There is no discount to apply.")
-            return "No discount applied."
+        else:
+            self.total = round(self.total - (self.total * (self.discount / 100)))
+            print(f"After the discount, the total comes to ${self.total}.")
 
     def void_last_transaction(self):
-        if self.items:  
-            last_transaction = self.items.pop()  
-            self.total -= last_transaction[1] * last_transaction[2] 
-            # self.total -= last_transaction[1] * last_transaction[2] 
-        else:
-            return "No transactions to void."
-
-
+        self.total -= self.last_transaction
